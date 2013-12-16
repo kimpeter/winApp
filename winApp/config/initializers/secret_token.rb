54c7117c358a74fa454aeaaba11d49e9fc5ec8ac
kimpeter <winkim7@gmail.com>
2013-12-17 05:22:31 +0900

    이제시작

---
+++ winApp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WinApp::Application.config.secret_key_base = '75489949a8526e2cf56c83e9607197409efdb53e4897527be9b3e7c58c672fa520d6155a4a841536c0ce73df47054fb21ce9ff7df43f66c34b9f23bb27d82046'
---
+++ winApp/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WinApp::Application.config.session_store :cookie_store, key: '_winApp_session'
